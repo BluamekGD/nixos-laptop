5a1cf06a7c7dbb5e43c714f788c10842a1867973
bluamek <bartosz.nicely@gmail.com>
2026-05-11 21:24:32 +0200

    forgot about this

--- flake.nix
+++ flake.nix
@@ -7,13 +7,8 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hyprland = {
-      url = "github:hyprwm/Hyprland";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
-
-  outputs = { self, nixpkgs, home-manager, hyprland, ... }: {
+  outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit hyprland; };
